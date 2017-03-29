8e9b71d0d2d71d840dfff5e16ea67924c65c6611
doomibox <weikaihu@Weikais-Mac-mini.local>
2017-03-29 12:02:00 -0700

    Updated UI to delay 2 seconds for user to restart game; Add sound to laser ejection

---
+++ Library/Collab/Download/Assets/Judge.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Judge : MonoBehaviour {
+
+	private static int player1Score;
+	private static int player2Score;
+	//private static float highScore;
+	private Text judgeText;		//display which player win
+	private Text player1Count;	//display player1's score
+	private Text player2Count;	//display player2's score
+	private Text mileage;
+	private float meters;
+	private GameObject scoreBoard;
+	private float speed;
+	private bool died;			//true when one player dies (reaches the judge block) in this turn
+	private float deadTime;
+
+	void Start () {
+		scoreBoard = GameObject.Find ("ScoreBoard");
+		judgeText = GameObject.Find ("JudgeText").GetComponent<Text> ();
+		player1Count = GameObject.Find ("Player1Count").GetComponent<Text> ();
+		player2Count = GameObject.Find ("Player2Count").GetComponent<Text> ();
+		mileage = GameObject.Find ("Mileage").GetComponent<Text> ();
+
+		scoreBoard.SetActive(false);
+		died = false;
+		meters = 0;
+
+	}
+
+	void Update() {
+		
+		if (died) {
+			if (Input.anyKeyDown && Time.time - deadTime >= 2f) {
+				gameReset ();
+			}
+		} else {
+			SetMileage ();
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player 1" && !died) {
+			player2Score++;
+			SetText (2);	//update player 2's scoreboard
+			died = true;
+			deadTime = Time.time;
+		}
+		if (coll.gameObject.tag == "Player 2" && !died) {
+			player1Score++;
+			SetText (1);	//update player 1's scoreboard
+			died = true;
+			deadTime = Time.time;
+		}
+
+		Invoke ("AskContinue", 2f);
+	}
+
+	void gameReset() {
+		scoreBoard.SetActive(false);		//stop showing the scores before restarting
+		SceneManager.LoadScene ("GamePlay");	//restart
+	}
+
+	void SetMileage() {
+		speed = GameObject.Find ("static block").GetComponent<StaticBlockMovement> ().blockSpeed;
+		meters += 2f * speed * Time.deltaTime;
+		mileage.text = meters.ToString("F2") + " M";	//round to 2 decimals
+	}
+
+	void SetText(int i) {
+		player1Count.text = "" + player1Score;
+		player2Count.text = "" + player2Score;
+		judgeText.text = "P L A Y E R  " + i + "  W O N";
+		scoreBoard.SetActive(true);	//enable after all changes are done
+	}
+
+	void AskContinue() {
+		judgeText.text = "P R E S S  A N Y K E Y \nT O\nC O N T I N U E";
+	}
+}
